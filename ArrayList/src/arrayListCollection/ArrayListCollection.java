3418c7d8a5c921929e30aba220f0be61fb1758bf
vpassos <viniciusmpassos2@gmail.com>
2015-02-25 09:52:56 -0300

    Comitar projeto ArrayList

---
+++ ArrayList/src/arrayListCollection/ArrayListCollection.java
@@ -0,0 +1,22 @@
+package arrayListCollection;
+
+import java.util.ArrayList;
+
+public class ArrayListCollection {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> items = new ArrayList<String>();
+		
+		items.add("red");
+		items.add(0,"yellow");
+		
+		System.out.print("Display list contents with counter-controlled loop");
+		
+		
+	}
+
+}
